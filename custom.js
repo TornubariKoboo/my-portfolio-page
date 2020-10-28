9da9fb96bee15c6a218ca36424488365fbb50e57
Tornubari <t.koboo12@gmail.com>
2020-10-28 01:39:31 -0400

    click on card will send to project page

---
+++ custom.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("#button").click(function () {
+        var url =" https://tornubarikoboo.github.io/Smith-s-Adventure/"
+        window.open(url);
+    });
+});
